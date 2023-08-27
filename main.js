0c14e3d8338f94523f8cd6ce4a3667d4a2fb6718
AlexNdungu <alexmeta517@gmail.com>
2023-08-27 19:39:04 +0300

    create electron app

---
+++ main.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: 'Image Resizer',
+    width: 800,
+    height: 600,
+  });
+
+  mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+}
+
+app.whenReady().then(()=> {
+    createMainWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+          createMainWindow();
+        }
+      });
+
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
