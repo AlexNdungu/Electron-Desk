2e050be3c4d945d9b3b56996ac42f8c9c9f5c4fb
AlexNdungu <alexmeta517@gmail.com>
2023-08-27 19:57:38 +0300

    create electron app

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const path = require('path');
 const { app, BrowserWindow } = require('electron');
+const isDev = process.env.NODE_ENV !== 'development';
 
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: 'Image Resizer',
-    width: 800,
-    height: 600,
+    width: isDev ? 1000 : 800,
+    height: 800,
   });
+
+  // Open Dev tools if in dev environment
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
   mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
